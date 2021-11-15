64d87493515ea8ca8abbff9ced3069417af2e094
tholzheim <tim.holzheim@rwth-aachen.de>
2021-11-15 01:00:21 +0100

    Fixed missing wikiusers

--- tests/basetest.py
+++ tests/basetest.py
@@ -2,10 +2,12 @@
 Created on 15.11.2021
 @author: tholzheim
 '''
+import os
 import unittest
 import time
 
 from corpus.eventcorpus import EventCorpus
+from wikibot.wikiuser import WikiUser
 
 
 class Profiler:
@@ -42,6 +44,7 @@ class BaseTest(unittest.TestCase):
 
 
     def setUp(self,debug:bool=False,profile:bool=True):
+        SmwHelper.createMissingWikiUsers()
         self.debug=debug
         self.profile=profile
         msg=f"test {self._testMethodName}, debug={self.debug}"
@@ -52,6 +55,50 @@ class BaseTest(unittest.TestCase):
     def tearDown(self):
         self.profiler.time()
         pass
+
+
+class SmwHelper:
+    """
+    Provides functions to generate missing wikiuser entries
+    """
+
+    @classmethod
+    def createMissingWikiUsers(cls):
+        neededWikiIds = ["or", "orclone"]
+        for wikiId in neededWikiIds:
+            cls.getSMW_WikiUser(wikiId=wikiId, save=True)
+
+
+    @classmethod
+    def getSMW_WikiUser(cls, wikiId="or", save=False):
+        '''
+        get semantic media wiki users for SemanticMediawiki.org and openresearch.org
+        '''
+        iniFile = WikiUser.iniFilePath(wikiId)
+        wikiUser = None
+        if not os.path.isfile(iniFile):
+            wikiDict = None
+            if wikiId == "or":
+                wikiDict = {"wikiId": wikiId, "email": "noreply@nouser.com", "url": "https://www.openresearch.org",
+                            "scriptPath": "/mediawiki/", "version": "MediaWiki 1.31.1"}
+            if wikiId == "orclone":
+                wikiDict = {"wikiId": wikiId, "email": "noreply@nouser.com",
+                            "url": "https://confident.dbis.rwth-aachen.de", "scriptPath": "/or/",
+                            "version": "MediaWiki 1.35.1"}
+            if wikiId == "cr":
+                wikiDict = {"wikiId": wikiId, "email": "noreply@nouser.com", "url": "https://cr.bitplan.com",
+                            "scriptPath": "/", "version": "MediaWiki 1.33.4"}
+
+            if wikiDict is None:
+                raise Exception("wikiId %s is not known" % wikiId)
+            else:
+                wikiUser = WikiUser.ofDict(wikiDict, lenient=True)
+                if save:
+                    wikiUser.save()
+        else:
+            wikiUser = WikiUser.ofWikiId(wikiId, lenient=True)
+        return wikiUser
+
 
 
 
